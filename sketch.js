37eee06536c5e11afa49e0eddcb7d9ae715e6b72
Holly Pham <hpham45@lsu.edu>
2024-08-28 15:25:28 -0500

    jhjkhj

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(100, 100);
+    background(0);
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(width/2,height/2, 30, 40);
+    fill(255, 130, 100);
+    ellipse(width/5,height/4, 20, 40);
+}
