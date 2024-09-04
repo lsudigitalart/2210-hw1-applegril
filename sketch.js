fe2e038eb8de93662ca0324f484acb4c982ec64f
Holly Pham <hpham45@lsu.edu>
2024-09-03 20:37:16 -0500

    kirby

--- sketch.js
+++ sketch.js
@@ -1,9 +1,36 @@
 function setup() {
-    createCanvas(100, 100);
-    background(0);
-    fill(255, 0, 0);
-    noStroke();
-    ellipse(width/2,height/2, 30, 40);
-    fill(255, 130, 100);
-    ellipse(width/5,height/4, 20, 40);
+    createCanvas(500, 500);
+    background(7,196,230);
+    noStroke()
+    fill(152, 25, 61);
+    ellipse(100, 400, 200, 100);
+    fill(152, 25, 61);
+    ellipse(400, 400, 200, 100);
+    fill(252, 96, 166);
+    ellipse(width/2,height/2, 500, 400);
+    fill(252, 96, 166);
+    ellipse(55, 100, 100, 50);
+    fill(252, 96, 166);
+    ellipse(445, 100, 100, 50);
+    fill(0);
+    ellipse(200, 110, 40, 100);
+    fill(0);
+    ellipse(300, 110, 40, 100);
+    fill(84, 71, 163);
+    ellipse(200, 110, 35, 95);
+    fill(84, 71, 163);
+    ellipse(300, 110, 35, 95);
+    fill(0);
+    ellipse(200, 97, 35, 70);
+    fill(0);
+    ellipse(300, 97, 35, 70);
+    fill(250);
+    ellipse(200, 85, 30, 40);
+    fill(250);
+    ellipse(300, 85, 30, 40);
+    fill(152, 25, 61)
+    ellipse(250, 300, 300, 250);
+    fill(255, 184, 176);
+    ellipse(250, 400, 100, 50)
+   
 }
